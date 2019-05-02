aa641de58dbeba38d397579479aa7198601ae934
Mykola Martynov <mykola.martynov@hotmail.com>
2019-05-03 00:54:47 +0300

    add test that dinosaur do not shrinked

--- tests/AppBundle/Entity/DinosaurTest.php
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -17,4 +17,13 @@ class DinosaurTest extends TestCase
 
         $this->assertSame(9, $dinosaur->getLength());
     }
+
+    public function testDinosaurHasNotShrunk()
+    {
+        $dinosaur = new Dinosaur();
+
+        $dinosaur->setLength(15);
+
+        $this->assertGreaterThan(12, $dinosaur->getLength(), 'Did you put it in the washing machine?');
+    }
 }
