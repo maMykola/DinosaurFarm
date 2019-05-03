f2473d8e347d208791f3847a0d64529e6188ca4d
Mykola Martynov <mykola.martynov@hotmail.com>
2019-05-03 21:34:24 +0300

    testing exceptions

--- src/AppBundle/Entity/Enclosure.php
+++ src/AppBundle/Entity/Enclosure.php
@@ -4,6 +4,7 @@
 namespace AppBundle\Entity;
 
 
+use AppBundle\Exception\NotABuffetException;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -36,6 +37,16 @@ class Enclosure
 
     public function addDinosaur(Dinosaur $dinosaur)
     {
+        if (!$this->canAddDinosaur($dinosaur)) {
+            throw new NotABuffetException();
+        }
+
         $this->dinosaurs[] = $dinosaur;
+    }
+
+    private function canAddDinosaur(Dinosaur $dinosaur): bool
+    {
+        return count($this->dinosaurs) === 0
+            || $this->dinosaurs->first()->isCarnivorous() === $dinosaur->isCarnivorous();
     }
 }
