564dcf029b6606b259fea6c700b257cb35dabe2e
Mykola Martynov <mykola.martynov@hotmail.com>
2019-05-04 21:56:03 +0300

    testing a form submit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Enclosure;
+use AppBundle\Factory\DinosaurFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,5 +21,40 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
             'enclosures' => $enclosures,
         ]);
+    }
+
+    /**
+     * @param Request         $request
+     * @param DinosaurFactory $dinosaurFactory
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws \AppBundle\Exception\DinosaursAreRunningRampantException
+     * @throws \AppBundle\Exception\NotABuffetException
+     *
+     * @Route("/grow", name="grow_dinosaur", methods={"POST"})
+     */
+    public function growAction(Request $request, DinosaurFactory $dinosaurFactory)
+    {
+        $manager = $this->getDoctrine()->getManager();
+
+        /** @var Enclosure $enclosure */
+        $enclosure = $manager->getRepository(Enclosure::class)
+            ->find($request->request->get('enclosure'));
+
+        $specification = $request->request->get('specification');
+        $dinosaur = $dinosaurFactory->growFromSpecification($specification);
+
+        $dinosaur->setEnclosure($enclosure);
+        $enclosure->addDinosaur($dinosaur);
+
+        $manager->flush();
+
+        $this->addFlash('success', sprintf(
+            'Grew a %s in enclosure #%d',
+            mb_strtolower($specification),
+            $enclosure->getId()
+        ));
+
+        return $this->redirectToRoute('homepage');
     }
 }
